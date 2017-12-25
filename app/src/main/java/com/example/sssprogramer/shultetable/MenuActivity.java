d5d80e3d64ec027e91c10767b4a2ab2e69379010
Stepan Smirnov <sssprogramer@gamil.com>
2017-12-25 10:11:29 +0300

    Add help activity

--- app/src/main/java/com/example/sssprogramer/shultetable/MenuActivity.java
+++ app/src/main/java/com/example/sssprogramer/shultetable/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.sssprogramer.shultetable;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void onHelpClick(View view) {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
     }
 }
